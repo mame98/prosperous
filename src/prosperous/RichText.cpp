57d8d5e5430bf1f919b07f323219c58aba9c46c2
Marius Messerschmidt <marius.messerschmidt@googlemail.com>
2023-10-14 16:33:55 +0200

    RichText: Refactor rich text rendering

--- src/prosperous/RichText.cpp
+++ src/prosperous/RichText.cpp
@@ -22,15 +22,73 @@ namespace prosperous {
         markup_spans.emplace_back(start, end, markup);
     }
 
-    struct SpanChange {
-        const Span* span;
-        bool is_end;
-        size_t location;
+    namespace {
 
-        bool operator<(const SpanChange& other) const {
-            return location < other.location;
+        struct SpanChange {
+            const Span* span;
+            bool is_end;
+            size_t location;
+
+            bool operator<(const SpanChange& other) const {
+                return location < other.location;
+            }
+        };
+
+        std::vector<SpanChange> compute_merged_spans(const std::vector<Span>& spans) {
+            std::vector<SpanChange> merged_spans{};
+            merged_spans.reserve(spans.size() * 2);
+
+            for(const auto& span : spans) {
+                SpanChange start{&span, false, span.start};
+                SpanChange stop{&span, true, span.end};
+                auto upper_bound_start = std::upper_bound(merged_spans.begin(), merged_spans.end(), start);
+                merged_spans.insert(upper_bound_start, start);
+
+                auto upper_bound_stop = std::upper_bound(merged_spans.begin(), merged_spans.end(), stop);
+                merged_spans.insert(upper_bound_stop, stop);
+            }
+
+            return merged_spans;
         }
-    };
+
+        std::vector<RichText> compute_subtexts(TextMarkup base_markup, const std::string& text, const std::vector<SpanChange>& merged_spans) {
+            std::vector<RichText> subtexts{};
+            std::vector<const Span*> current_spans{};
+            size_t last_location = 0;
+            for(const auto& span_change : merged_spans) {
+
+
+                auto affected_substring = text.substr(last_location, span_change.location - last_location);
+                if(!affected_substring.empty()) {
+                    TextMarkup markup = base_markup;
+                    for(const auto* span : current_spans) {
+                        markup.update(span->markup);
+                    }
+                    subtexts.emplace_back(affected_substring, markup);
+                }
+
+                if(!span_change.is_end) {
+                    current_spans.push_back(span_change.span);
+                }
+                if(span_change.is_end) {
+                    current_spans.erase(std::remove(current_spans.begin(), current_spans.end(), span_change.span), current_spans.end());
+                }
+
+                last_location = span_change.location;
+            }
+
+            auto remaining_string = text.substr(last_location);
+            TextMarkup markup = base_markup;
+            for(const auto* span : current_spans) {
+                markup.update(span->markup);
+            }
+
+            subtexts.emplace_back(remaining_string, markup);
+            return subtexts;
+        }
+
+
+    }
 
     std::string RichText::render() const {
 
@@ -38,57 +96,9 @@ namespace prosperous {
             return colorize_string(text, ColorMode::TRUE_COLOR_24BIT, base_markup.foreground, base_markup.background);
         }
 
-        /* first pass: creat a vector of merged spans */
-        std::vector<SpanChange> merged_spans{};
-        merged_spans.reserve(markup_spans.size() * 2);
+        auto merged_spans = compute_merged_spans(this->markup_spans);
+        auto subtexts = compute_subtexts(this->base_markup, this->text, merged_spans);
 
-        for(const auto& span : markup_spans) {
-            SpanChange start{&span, false, span.start};
-            SpanChange stop{&span, true, span.end};
-            auto upper_bound_start = std::upper_bound(merged_spans.begin(), merged_spans.end(), start);
-            merged_spans.insert(upper_bound_start, start);
-
-            auto upper_bound_stop = std::upper_bound(merged_spans.begin(), merged_spans.end(), stop);
-            merged_spans.insert(upper_bound_stop, stop);
-        }
-
-        /* second pass: iterate over merged spans and create a vector of RichText attributes with no spans */
-        std::vector<RichText> subtexts{};
-        std::vector<const Span*> current_spans{};
-        size_t last_location = 0;
-        for(const auto& span_change : merged_spans) {
-
-
-            auto affected_substring = text.substr(last_location, span_change.location - last_location);
-            if(!affected_substring.empty()) {
-                TextMarkup markup = base_markup;
-                for(const auto* span : current_spans) {
-                    markup.update(span->markup);
-                }
-                subtexts.emplace_back(affected_substring, markup);
-            }
-
-            if(!span_change.is_end) {
-                current_spans.push_back(span_change.span);
-            }
-            if(span_change.is_end) {
-                current_spans.erase(std::remove(current_spans.begin(), current_spans.end(), span_change.span), current_spans.end());
-            }
-
-            last_location = span_change.location;
-        }
-
-        auto remaining_string = text.substr(last_location);
-        TextMarkup markup = base_markup;
-        for(const auto* span : current_spans) {
-            markup.update(span->markup);
-        }
-
-        subtexts.emplace_back(remaining_string, markup);
-
-
-
-        /* third pass: join all subtexts, formatted */
         std::string result{};
         for(const auto& subtext : subtexts) {
             result += subtext.render();
