85f278aa534576d7848eff78a6f72e10098c1da5
Marius Messerschmidt <marius.messerschmidt@googlemail.com>
2023-10-20 00:04:25 +0200

    Meassurement: Add glyph counter

---
+++ src/prosperous/Meassurement.hpp
@@ -0,0 +1,12 @@
+#ifndef PROSPEROUS_MEASSUREMENT_HPP
+#define PROSPEROUS_MEASSUREMENT_HPP
+
+#include <string>
+
+namespace prosperous {
+
+  size_t get_glyph_count(std::string string);
+
+} // prosperous
+
+#endif //PROSPEROUS_MEASSUREMENT_HPP
